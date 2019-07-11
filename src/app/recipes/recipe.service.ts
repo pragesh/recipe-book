fd260acc3cbffde68d3e3b816a9c7fd2d68a2664
pragesh <pragesh4jagnani@gmail.com>
2019-07-11 16:52:18 -0400

    Creating the edit recipe using reactive forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,8 +2,10 @@ import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
+    recipeChanged = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe(
             'A test Recipe', 
@@ -32,4 +34,18 @@ export class RecipeService {
         return this.recipes[index];
     }
 
+    addRecipe(recipe : Recipe) {
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe : Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index : number) {
+        this.recipes.splice(index, 1);
+        this.recipeChanged.next(this.recipes.slice());
+    }
 }
