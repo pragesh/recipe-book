8c0f51eed6d30021c9c85840aca7cbdd78c7c2d9
Pragesh Jagnani <jagnanip@jagnanip-a01.vmware.com>
2019-07-11 12:00:06 -0400

    Creating a form using TD approach

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -8,6 +8,7 @@ export class ShoppingListService {
       ];
     
     ingredientChanged  = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     getIngredients() {
         return this.ingredients.slice();
@@ -20,6 +21,20 @@ export class ShoppingListService {
 
     addIngredients(ingredients : Ingredient[]) {
         this.ingredients.push(...ingredients);
+        this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
+    updateIngredient(index: number, newIngredient : Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientChanged.next(this.ingredients.slice());
     }
 
